bc8436ce89d98f8a33b70025dc652463d0056070
郑茁 <zze1009@infypower.cn>
2024-11-27 13:29:19 +0800

    初始化

---
+++ src/main/java/cn/edu/wic/exam/courseadmin/CourseAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.wic.exam.courseadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/wic/exam/courseadmin/CourseAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.wic.exam.courseadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
